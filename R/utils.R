d45bccad51c00aeff0c2097c66c2e147c3687254
dgrapov <dgrapov@gmail.com>
2021-10-03 10:45:18 -0700

    update docs

--- R/utils.R
+++ R/utils.R
@@ -5,26 +5,6 @@ init_ocpu<-function(open_cpu_url='http://localhost/ocpu/'){
   options('open_cpu_url' = open_cpu_url)
 }
 
-#' #generic function combining main fun and json parsing
-#' #' @title dave_fun_toJSON
-#' #' @export
-#' dave_fun_toJSON<-function(fun,args){
-#'
-#'   res<-do.call(fun,args)
-#'
-#'   ocpu_toJSON(res)
-#'
-#' }
-
-##' @title opcpu_fun
-##' @export
-# ocpu_fun<-function(fun,...){
-#
-#   function(...){
-#     ocpu_toJSON(do.call(fun,...))
-#   }
-# }
-
 #keep object class when forcing toJSON
 #' @title dave_toJSON
 #' @export
@@ -34,9 +14,9 @@ ocpu_toJSON<-function(x,...){
   .class<-class(x)
   check<-tryCatch(toJSON(x),error = function(e){NULL})
 
-  if(!is.null(check)) return(list(results=check,class=.class))
+  if(!is.null(check)) return(list(results=check,ocpu_class=.class))
   .class<-class(x)
-  list(results = toJSON(x,force=TRUE),dave_ocpu_class=.class)
+  list(results = toJSON(x,force=TRUE),ocpu_class=.class)
 
 }
 
@@ -45,34 +25,35 @@ ocpu_toJSON<-function(x,...){
 #' @import jsonlite
 ocpu_fromJSON<-function(x){
 
-  if(x$dave_ocpu_class %in% 'data.frame'){
+  if(x$ocpu_class %in% 'data.frame'){
     fromJSON(x$results) %>% as.data.frame()
-  }
+  } else
 
   obj<-x$results %>% fromJSON(.)
-  class(obj)<-x$dave_ocpu_class
+  class(obj)<-x$ocpu_class
   obj
 }
 
 
-.ocpu_post<-function(fun,body,pkg_url = getOption('dave.stat_url'), base_url=getOption('open_cpu_url')){
+ocpu_post<-function(fun,body,pkg_url, base_url=getOption('open_cpu_url')){
 
   url<-paste0(base_url,pkg_url,fun)
   post_ocpu(url=url,body=body)
 }
 
-#' @title ocpu_post
-#' @export
-#' @import memoise
-#' @details can get fails to memoize if called from a shiny app
-ocpu_post<-memoise::memoise(.ocpu_post)
+# seems memoization should be done at run time
+##' @title ocpu_post
+##' @export
+##' @import memoise
+##' @details can get fails to memoize if called from a shiny app
+#ocpu_post<-memoise::memoise(.ocpu_post)
 
 
 #' @export
 #' @details used to serialize diverse classes to json
 ocpu_fun<-function(fun,...){
-  do.call(fun,...) %>%
-    ocpu_toJSON()
+  tmp<-do.call(fun,...)
+  ocpu_toJSON(tmp)
 }
 
 #' @title ocpu_call
@@ -80,21 +61,17 @@ ocpu_fun<-function(fun,...){
 #' @details call function and serialize results to json
 ocpu_call <-
   function(fun,
-           body=NULL,
-           pkg_url=NULL,
+           body = NULL,
+           pkg_url = NULL,
            base_url = getOption('open_cpu_url')) {
-
     results <- error <- NULL
 
     out <- tryCatch(
-      list(
-        results = ocpu_post(
+       ocpu_post(
           fun = fun,
           body = body,
           pkg_url = pkg_url,
           base_url = base_url
-        ),
-        error = error
       ),
       error = function(e) {
         list(results = results, error = as.character(e))
@@ -102,34 +79,12 @@ ocpu_call <-
     )
 
 
-  .call<-tryCatch(out$results  %>% ocpuclient::ocpu_fromJSON(),error=function(e){as.character(e)})
+   #serialize class
+    if(!is.null(out$results$ocpu_class)){
+      out$results<- ocpu_fromJSON(out$results)
+    }
 
-  list(results=.call,error=c(out$error,.call$error))
+    return(out)
 
   }
-
-test<-function(){
-
-  library(ocpuclient)
-  library(dave.network)
-
-  # .ocpu_metabolic_network<-ocpuclient::ocpu_fun('metabolic_network')
-
-  #' @title ocpu_metabolic_network
-  #' @export
-  ocpu_metabolic_network <- function(
-    body,
-    pkg_url = getOption('dave.network_url'),
-    base_url = getOption('open_cpu_url')) {
-
-    f<-dave.network::metabolic_network
-    fun<-ocpuclient::ocpu_fun('f')
-    ocpu_call(fun,
-              body,
-              pkg_url,
-              base_url)
-  }
-
-
-}
 
--- README.md
+++ README.md
@@ -8,14 +8,66 @@ output:
 
 > ## Simple [`opencpu`](https://www.opencpu.org/) API client
 
-
-
 ### Installation
 
 ```r
 install_github("dgrapov/ocpuclient")
 ```
 
-### Useful functions
-* `ocpu_post` - memoised `POST` to open cpu server and combined `GET` of results
-* `ocpu_fun_toJSON` - call function and return results as json including custom classes
+### Summary
+The purpose of this package is to create drop in replacements for native library functions to instead call the libraries' opencpu endpoints. This can be used to create a scalable microservice infrastructure to support many functions diverse compute requirements while reducing client dependancy imports.
+
+### Example
+The following describes an example workflow for creating an opencpu client function for package `foo_bar`.
+
+Native function in library `foo_bar`
+
+```r
+
+#' @title foo
+#' @export
+foo<-function(...){
+  paste0('That is ', ifelse(sample(c(FALSE,TRUE), size=1),'bar','foo'))
+}
+
+```
+
+Wrapper function in library `foo_bar` to handle custom results serialization to json, etc
+
+```r
+.foo<-function(...){
+  ocpu_toJSON(foo(...))
+}
+
+```
+
+Client library `foo_bar.client` function for  `foo_bar::foo`
+
+```r
+
+#' @title ocpu_foo
+#' @import ocpuclient
+ocpu_foo  <- function(fun = '.foo',
+                      body = NULL,
+                      pkg_url = 'library/foo/R/',
+                      base_url = 'http://localhost/ocpu/') {
+  ocpu_call(fun,
+            body,
+            pkg_url,
+            base_url)
+}
+
+```
+
+Use drop in replacement opencpu endpoint call
+
+```r
+
+library(ocpuclient)
+
+body<-list(arg1=1)
+
+ocpu_foo(body=body)
+
+```
+
